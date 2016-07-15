36e165ef23f7d56e35f6a50a28b282dfe0ef798b
Mark Kastner <Mark Kastner>
2016-07-15 18:15:30 +0200

    Berechtigungen - Standort - Map

--- PokemonGoLocation/app/src/main/java/com/kastner/richter/pokemongolocation/MainActivity.java
+++ PokemonGoLocation/app/src/main/java/com/kastner/richter/pokemongolocation/MainActivity.java
@@ -1,13 +1,102 @@
 package com.kastner.richter.pokemongolocation;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Criteria;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.Build;
+import android.provider.Settings;
+import android.provider.SyncStateContract;
+import android.support.annotation.RequiresPermission;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.util.List;
+
+public class MainActivity  extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    private double longitude = 0.0;
+    private double latitude = 0.0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+        int MyVersion = Build.VERSION.SDK_INT;
+        if (MyVersion > Build.VERSION_CODES.LOLLIPOP_MR1) {
+            if (!checkIfAlreadyhavePermission()) {
+                requestForSpecificPermission();
+            }
+        }
+        LocationService ls = new LocationService(this);
+        this.longitude = ls.getLongitude();
+        this.latitude = ls.getLatitude();
     }
+
+    private boolean checkIfAlreadyhavePermission() {
+        int result = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION);
+        if (result == PackageManager.PERMISSION_GRANTED) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private void requestForSpecificPermission() {
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 101);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case 101:
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    //granted
+                    LocationService ls = new LocationService(this);
+                    this.longitude = ls.getLongitude();
+                    this.latitude = ls.getLatitude();
+                } else {
+                    //not granted
+                    Toast.makeText(this, "Ohne Standort funktioniert diese App nicht. Bitte in den Einstellungen Standort zulassen.", Toast.LENGTH_LONG).show();
+                    System.out.println("Keine Erlaubnis!");
+                }
+                break;
+            default:
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng currentPosition = new LatLng(this.latitude, this.longitude);
+        mMap.addMarker(new MarkerOptions().position(currentPosition).title("Deine Position"));
+       // mMap.moveCamera(CameraUpdateFactory.newLatLng(currentPosition));
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(currentPosition,16));
+    }
+
 }
